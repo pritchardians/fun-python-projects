b71b98d1a84c027ca615abe3121b5d2fece5f7a9
Ian Pritchard <pritchardians@gmail.com>
2021-12-06 17:27:51 -0600

    start to dram etcha-a-sketch machine

--- etch-a-sketch/etch-a-sketch.py
+++ etch-a-sketch/etch-a-sketch.py
@@ -2,7 +2,17 @@ import turtle
 
 pict = turtle.Turtle()
 
-pict.forward(10)
+pict = turtle.Turtle()
+pict.speed(10)
+pict.shape('circle')
+pict.color('red')
+pict.width(3)
+pict.turtlesize(0.25)
+
+pict.penup()
+pict.goto(-230, -40)
+pict.down()
+pict.forward(450)
 
 
 '''
