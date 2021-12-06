38bf690e7845098a1ca0a09780e3d05282cee013
Ian Pritchard <pritchardians@gmail.com>
2021-12-06 17:46:19 -0600

    added outer box

--- etch-a-sketch/etch-a-sketch.py
+++ etch-a-sketch/etch-a-sketch.py
@@ -1,7 +1,9 @@
 import turtle
 
 SCREEN_WIDTH = 450
+ETCH_A_SKETCH_WIDTH = SCREEN_WIDTH + 50
 SCREEN_HEIGHT = 250
+ETCH_A_SKETCH_HEIGHT = SCREEN_HEIGHT + 100
 
 pict = turtle.Turtle()
 pict.speed(10)
@@ -10,6 +12,7 @@ pict.color('red')
 pict.width(3)
 pict.turtlesize(0.25)
 
+# Draw inner screen
 pict.penup()
 pict.goto(-230, -40)
 pict.down()
@@ -20,6 +23,21 @@ pict.left(90)
 pict.forward(SCREEN_WIDTH)
 pict.left(90)
 pict.forward(SCREEN_HEIGHT)
+
+# Draw edge of machine
+pict.penup()
+pict.goto(-250, -110)
+pict.pendown()
+pict.width(5)
+
+pict.left(90)
+pict.forward(ETCH_A_SKETCH_WIDTH)
+pict.left(90)
+pict.forward(ETCH_A_SKETCH_HEIGHT)
+pict.left(90)
+pict.forward(ETCH_A_SKETCH_WIDTH)
+pict.left(90)
+pict.forward(ETCH_A_SKETCH_HEIGHT)
 
 
 '''
