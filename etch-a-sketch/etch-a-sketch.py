98859431b92275e0616e2de938ecb282590f6f69
Ian Pritchard <pritchardians@gmail.com>
2021-12-06 19:12:26 -0600

    added lettering

--- etch-a-sketch/etch-a-sketch.py
+++ etch-a-sketch/etch-a-sketch.py
@@ -43,6 +43,30 @@ pict.circle(OUTER_CIRCLE, QUARTER_CIRCLE)
 pict.forward(ETCH_A_SKETCH_HEIGHT)
 pict.circle(OUTER_CIRCLE, QUARTER_CIRCLE)
 
+# Draw the knobs
+pict.penup()
+pict.goto(-250, -140)
+pict.pendown()
+
+pict.color('gray')
+pict.width(4)
+pict.circle(35)
+pict.penup()
+pict.forward(SCREEN_WIDTH + 50)
+pict.pendown()
+pict.circle(35)
+
+# Draw the Etch A Sketch letters
+pict.penup()
+pict.left(180)
+pict.forward(250)
+pict.right(90)
+pict.forward(25)
+pict.pendown()
+
+pict.color('blue')
+style = ('Arial', 30, 'bold italic')
+pict.write('Etch A Sketch', font=style, align='center')
 
 '''
 Make this the last part of the code!
