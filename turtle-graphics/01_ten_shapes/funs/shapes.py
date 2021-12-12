13f42a6d8f0de1c11330ab781217c65041b912e9
Ian Pritchard <pritchardians@gmail.com>
2021-12-12 09:20:22 -0600

    completed turtle ten_shapes

--- turtle-graphics/01_ten_shapes/funs/shapes.py
+++ turtle-graphics/01_ten_shapes/funs/shapes.py
@@ -1,17 +1,17 @@
 import random
 
 
-def random_colour(pic):
-    c_red = random.randint(0, 100) / 100
-    c_green = random.randint(0, 100) / 100
-    c_blue = random.randint(0, 100) / 100
-    pic.color(c_red, c_green, c_blue)
+def random_colour():
+    red = random.randint(0, 100) / 100
+    blue = random.randint(0, 100) / 100
+    green = random.randint(0, 100) / 100
+    return (red, blue, green)
 
 
 def draw_shape(pic, sides):
     pic.speed(sides)
     pic.width(sides * 2)
-    random_colour(pic)
+    pic.color(random_colour())
     degrees_to_turn = 360 / sides
     for _ in range(1, sides + 1):
         pic.left(degrees_to_turn)
